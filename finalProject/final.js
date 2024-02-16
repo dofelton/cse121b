81f26f1600dd6d9c8e65106e66fc4ce1a4896008
Don Felton <dofelton@gmail.com>
2024-02-16 15:23:12 -0600

    Final project

--- finalProject/final.js
+++ finalProject/final.js
@@ -6,37 +6,43 @@ let url = "https://run.mocky.io/v3/04b14e00-90a0-4fa7-a7dd-67b881b52deb"
 const getPizzas = async () => {
     const response = await fetch(url);
     if (response.ok) {
-        pizzas = await response.json();
+        pizzaList = await response.json();
     console.log(pizzaList);
+    displayPizzas(pizzaList);
+    input.focus();
     }
 }
+const choices = document.querySelector('#choices');
+const displayPizzas = (pizzas) => {
+    pizzas.forEach(function (pizza){
+        let name = document.createElement('h2');
+        name.textContent = pizza.name;
+        choices.append(name);
+    })
+}
 
-// const displayPizzas = (pizzas) => {
-//     pizzas.forEach(function (pizza){
-//         let name = document.createElement('h2');
-//         name.textContent = pizza.name
-//     })
-// }
+const input = document.querySelector('#recipe');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
 
-// const input = document.querySelector('#recipe');
-// const button = document.querySelector('button');
-// const list = document.querySelector('#list');
+button.addEventListener('click', () => {
+    if (input.value != '') {
+        const li = document.createElement('li');
+        li.textContent = `The ingredients for a ${input.value} pizza are:`;
+        list.append(li);
+        displayIngredients(pizzaList);
+    }
+})
 
-// button.addEventListener('click', () => {
-//     if (input.value != '') {
-//         const li = document.createElement('li');
-//         li.textContent = input.value;
-//         list.append(li);
-//         displayIngredients();
-//     }
-// })
+const displayIngredients = (pizza) => {
+    pizza.forEach(function (pizza) {
+        if (pizza.name === input.value) {
+            let li = document.createElement('li');
+            li.textContent = `${pizza.ingredients}`;
+            list.append(li);}
+    }
+        )
 
-// const displayIngredients = (pizza) => {
-//     // let ing = pizzas.pizza[input.value].ingredients;
-//     pizza.forEach(pizza => console.log(pizza))
-
-//     // )
-//     // ing.append(li);
-// }
+}
 
 getPizzas();
